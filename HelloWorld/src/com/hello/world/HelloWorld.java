4fbc19617372e6d07ed8efeef9ca1e77f04cf0c6
Dhanapal T <Dhanapal T@LAPTOP-JL4RV5N6>
2022-09-03 21:05:21 +0530

    initial commit

---
+++ HelloWorld/src/com/hello/world/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.hello.world;
+
+public class HelloWorld {
+public static void main(String[] args) {
+	System.out.println("this github sample program");
+	System.out.println("HelloWorld");
+}
+}
